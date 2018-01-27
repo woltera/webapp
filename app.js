abd59b750effd99bb5d91a6c4f257b32867086b4
martijn <woltera10@gmail.com>
2018-01-27 13:53:24 +0100

    port for heroku

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require ('express');	
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.set ('view engine', 'ejs');
 
@@ -11,6 +12,6 @@ app.get('/about', function(req, res){
 	res.render('about')
 });
 
-app.listen(3000, function(){
+app.listen(port, function(){
    console.log("simpele app");
 });
